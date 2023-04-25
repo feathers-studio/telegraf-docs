71ae06d4ffa0a5df13c0ec497341ca9ecedab182
romium <39771039+RomanPor@users.noreply.github.com>
2023-04-25 18:14:51 +0300

    Update fastify.ts
    Fixed invalid callbackUrl
    Fixed wrong webhook request param
    Added types

--- examples/webhook/fastify.ts
+++ examples/webhook/fastify.ts
@@ -1,12 +1,18 @@
-import { fastify } from "fastify";
+import { fastify, FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { Telegraf } from "telegraf";
+import {Update} from "typegram/update";
+
+type TWebhookReq = http.IncomingMessage & {body?: Update};
 
 const bot = new Telegraf(token);
-const app = fastify();
+const app: FastifyInstance = fastify();
 
 const webhook = await bot.createWebhook({ domain: webhookDomain });
+const callbackUrl = `/telegraf/${bot.secretPathComponent()}`;
 
-app.post(bot.secretPathComponent(), (req, rep) => webhook(req.raw, rep.raw));
+app.post(callbackUrl, (req: FastifyRequest, rep: FastifyReply) => {
+    return webhook({...req.raw, body: req.body} as TWebhookReq, rep.raw as http.ServerResponse);
+});
 
 bot.on("text", ctx => ctx.reply("Hello"));
 
